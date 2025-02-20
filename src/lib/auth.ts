f7b5c04e38ce66efbfd6c39f5e5c1682ddec2609
m-ali79 <muhammadali75931@gmail.com>
2025-02-21 02:09:43 +0500

    fix:not working scopes issues

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,41 +4,32 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const scopes = [
-  "email",  // Short form for userinfo.email
-  "profile", // Short form for userinfo.profile
-  "https://www.googleapis.com/auth/drive.metadata.readonly",
-];
-
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
 
   emailAndPassword: {
     enabled: true,
-    async sendResetPassword(data, request) {
+    async sendResetPassword(data, request) { },
+  },
 
-    }
-  }
-  ,
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          scope: scopes.join(' '),
-          prompt: "select_account",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
+      scope: [
+        "email",
+        "profile",
+        "https://www.googleapis.com/auth/drive.readonly",
+        "https://www.googleapis.com/auth/drive.activity.readonly",
+        "https://www.googleapis.com/auth/drive.metadata",
+      ],
     },
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-    }
+  },
+
+  github: {
+    clientId: process.env.GITHUB_CLIENT_ID as string,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
   },
 });
